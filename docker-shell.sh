5b3d1401e0658e137376af6bd0d277e838be2883
Shivas Jayaram <shivasj@gmail.com>
2023-09-06 16:12:05 -0400

    Base docker shell file

---
+++ docker-shell.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+
+set -e
+
+export BASE_DIR=$(pwd)
+export SECRETS_DIR=$(pwd)/../secrets/
+
+# Create the network if we don't have it yet
+docker network inspect data-versioning-network >/dev/null 2>&1 || docker network create data-versioning-network
+
+# Build the image based on the Dockerfile
+docker build -t data-version-cli --platform=linux/arm64/v8 -f Dockerfile .
+
+# Run Container
+docker run --rm --name data-version-cli -ti \
+-v "$BASE_DIR":/app \
+-v "$SECRETS_DIR":/secrets \
+-e GOOGLE_APPLICATION_CREDENTIALS=/secrets/data-service-account.json \
+-e GCP_PROJECT="ac215-project" \
+-e GCP_ZONE="us-central1-a" \
+--network data-versioning-network data-version-cli
