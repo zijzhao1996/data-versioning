b57892b73a4d90f6e5c295a7e566f0554ba7eb4a
Rashmi B <rjain29@gmail.com>
2023-09-16 22:46:17 -0400

    dataset updates...

--- docker-shell.sh
+++ docker-shell.sh
@@ -4,9 +4,9 @@ set -e
 
 export BASE_DIR=$(pwd)
 export SECRETS_DIR=$(pwd)/../secrets/
-export GCS_BUCKET_NAME="mushroom-app-data-demo"
-export GCP_PROJECT="ac215-project"
-export GCP_ZONE="us-central1-a"
+export GCS_BUCKET_NAME="ac215mushrooms"
+export GCP_PROJECT="siim-isic"
+export GCP_ZONE="us-east1-b"
 
 # Create the network if we don't have it yet
 docker network inspect data-versioning-network >/dev/null 2>&1 || docker network create data-versioning-network
@@ -19,7 +19,7 @@ docker run --rm --name data-version-cli -ti \
 -v "$BASE_DIR":/app \
 -v "$SECRETS_DIR":/secrets \
 -v ~/.gitconfig:/etc/gitconfig \
--e GOOGLE_APPLICATION_CREDENTIALS=/secrets/data-service-account.json \
+-e GOOGLE_APPLICATION_CREDENTIALS=/secrets/siim-isic-secret.json \
 -e GCP_PROJECT=$GCP_PROJECT \
 -e GCP_ZONE=$GCP_ZONE \
 -e GCS_BUCKET_NAME=$GCS_BUCKET_NAME \
